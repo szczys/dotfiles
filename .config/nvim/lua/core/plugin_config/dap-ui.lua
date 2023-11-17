09369942d147ca5f918329386573768165787d07
Mike Szczys <szczys@hotmail.com>
2023-11-17 09:00:17 -0600

    vim: nvim-dap-ui: add keybindings for debugger
    Signed-off-by: Mike Szczys <szczys@hotmail.com>

--- .config/nvim/lua/core/plugin_config/dap-ui.lua
+++ .config/nvim/lua/core/plugin_config/dap-ui.lua
@@ -11,3 +11,9 @@ dap.listeners.before.event_exited["dapui_config"] = function()
   dapui.close()
 end
 
+vim.cmd([[ command! DapUiToggle lua require("dapui").toggle() ]])
+
+vim.api.nvim_set_keymap("n", "<leader>dt", ":DapUiToggle<CR>", {noremap=true})
+vim.api.nvim_set_keymap("n", "<leader>db", ":DapToggleBreakpoint<CR>", {noremap=true})
+vim.api.nvim_set_keymap("n", "<leader>dc", ":DapContinue<CR>", {noremap=true})
+vim.api.nvim_set_keymap("n", "<leader>dr", ":lua require('dapui').open({reset = true})<CR>", {noremap=true})
