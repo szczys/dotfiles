4eed70c24be7bb50a0803dc59070c5bb52795eba
Mike Szczys <szczys@hotmail.com>
2024-04-26 22:54:14 -0500

    telescope: use <leader>ss to pull up spelling suggestions
    Signed-off-by: Mike Szczys <szczys@hotmail.com>

--- .config/nvim/lua/core/plugin_config/telescope.lua
+++ .config/nvim/lua/core/plugin_config/telescope.lua
@@ -6,6 +6,7 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, {})
 vim.keymap.set('n', '<leader>fw', builtin.grep_string, {})
 vim.keymap.set('n', '<leader>fj', builtin.jumplist, {})
+vim.keymap.set('n', '<leader>ss', builtin.spell_suggest, {})
 
 require('telescope').setup{
   defaults = {
