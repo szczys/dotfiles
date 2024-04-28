135b972fcde7b85f03d7f222cdffe15fcb0afab8
Mike Szczys <szczys@hotmail.com>
2024-04-28 00:26:11 -0500

    nvim: J/K moves visually selected lines up and down
    Signed-off-by: Mike Szczys <szczys@hotmail.com>

--- .config/nvim/lua/core/keymap.lua
+++ .config/nvim/lua/core/keymap.lua
@@ -17,6 +17,10 @@ vim.keymap.set("n", "<leader>Y", "\"+Y", { desc = "Yank into \" register" })
 vim.keymap.set("n", "<leader>d", "\"_d", { desc = "Delete into \" register" })
 vim.keymap.set("v", "<leader>d", "\"_d", { desc = "Delete into \" register" })
 
+-- Move lines up or down when in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 -- Keep window centered when going up/down
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
