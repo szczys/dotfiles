8578aa85430bac70763a44998cb20f68d70e0e88
Mike Szczys <szczys@hotmail.com>
2024-07-11 10:09:08 -0500

    nvim: enable theme integrations for catppuccin
    Signed-off-by: Mike Szczys <szczys@hotmail.com>

--- .config/nvim/lua/core/plugin_config/catppuccin.lua
+++ .config/nvim/lua/core/plugin_config/catppuccin.lua
@@ -1,1 +1,15 @@
 vim.cmd [[ colorscheme catppuccin ]]
+
+require("catppuccin").setup({
+    integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = false,
+        mini = {
+            enabled = true,
+            indentscope_color = ""
+        }
+    }
+})
