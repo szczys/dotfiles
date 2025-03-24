eca019d2b52744dcfe356e522ecfaa5983ee3e94
Mike Szczys <szczys@hotmail.com>
2025-03-24 10:16:43 -0500

    nvim: neotree: show dotfiles; automatically update tree
    Signed-off-by: Mike Szczys <szczys@hotmail.com>

--- .config/nvim/lua/core/plugin_config/nvim-neo-tree.lua
+++ .config/nvim/lua/core/plugin_config/nvim-neo-tree.lua
@@ -1,2 +1,12 @@
 -- Toggle NeoTree
 vim.keymap.set('n', '<C-n>', '<Cmd>Neotree toggle<CR>')
+
+require("neo-tree").setup({
+  filesystem = {
+    use_libuv_file_watcher = true,
+    filtered_items = {
+      hide_dotfiles = false,
+      hide_gitignored = true,
+    },
+  },
+})
