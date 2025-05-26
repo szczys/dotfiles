60e2e0cf37a78fe945854a259cde0566756b800a
Mike Szczys <szczys@hotmail.com>
2025-05-26 11:18:51 -0500

    fish: fix timeout for kj escape sequence
    Signed-off-by: Mike Szczys <szczys@hotmail.com>

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,9 @@
 function fish_user_key_bindings
   fish_vi_key_bindings
+
+  # Use k-j as escape
   bind -M insert -m default kj backward-char force-repaint
+
+  # timeout for key sequences (don't wait forever when 'k' was last key)
+  set -g fish_sequence_key_delay_ms 200
 end
