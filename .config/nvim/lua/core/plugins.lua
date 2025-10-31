ac80eab7c799f56323a6d759aa8a29c78aa5cf5e
Mike Szczys <szczys@hotmail.com>
2025-10-30 23:14:59 -0500

    nvim: add grapple plugin
    Signed-off-by: Mike Szczys <szczys@hotmail.com>

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -123,6 +123,23 @@ local plugins = {
       vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
     end
   },
+  {
+    "cbochs/grapple.nvim",
+    opts = {
+        scope = "git", -- also try out "git_branch"
+    },
+    event = { "BufReadPost", "BufNewFile" },
+    cmd = "Grapple",
+    keys = {
+        { "<leader>m", "<cmd>Grapple toggle<cr>", desc = "Grapple toggle tag" },
+        { "<leader>M", "<cmd>Grapple toggle_tags<cr>", desc = "Grapple open tags window" },
+        { "<leader>n", "<cmd>Grapple cycle_tags next<cr>", desc = "Grapple cycle next tag" },
+        { "<leader>p", "<cmd>Grapple cycle_tags prev<cr>", desc = "Grapple cycle previous tag" },
+    },
+    dependencies = {
+        { "nvim-tree/nvim-web-devicons", lazy = true }
+    },
+  },
 }
 
 local opts = {
