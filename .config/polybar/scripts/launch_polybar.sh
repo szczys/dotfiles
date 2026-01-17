5d9d0bbc1efc0ee60e121a74857e5fe38205be0a
Mike Szczys <szczys@hotmail.com>
2026-01-17 11:11:43 -0600

    remove i3 configurations
    Signed-off-by: Mike Szczys <szczys@hotmail.com>

--- .config/polybar/scripts/launch_polybar.sh
+++
@@ -1,8 +0,0 @@
-POLYBAR=~/compile/polybar/build/bin/polybar
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m $POLYBAR --reload toph &
-  done
-else
-  $POLYBAR --reload toph &
-fi
