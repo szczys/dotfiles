79e69a34062dbdaae8c51319180f7d4106663cf0
Mike Szczys <szczys@hotmail.com>
2026-03-13 16:41:03 -0500

    hypr: scripts: turn monitor on/off when running fix-workspaces
    Signed-off-by: Mike Szczys <szczys@hotmail.com>

--- .mydotfiles/com.ml4w.dotfiles/.config/hypr/scripts/fix-workspaces.sh
+++ .mydotfiles/com.ml4w.dotfiles/.config/hypr/scripts/fix-workspaces.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-
+hyprctl keyword monitor eDP-1,2560x1600,1280x1080,1
 hyprctl --instance 0 dispatch moveworkspacetomonitor 1 1
 hyprctl --instance 0 dispatch moveworkspacetomonitor 2 1
 hyprctl --instance 0 dispatch moveworkspacetomonitor 3 2
 hyprctl --instance 0 dispatch moveworkspacetomonitor 4 2
+hyprctl keyword monitor eDP-1, disable
