b1662c2fe95fbbb3f009e9b9d8803085040e3c2f
Mike Szczys <szczys@hotmail.com>
2026-04-29 22:08:50 -0500

    nvim: add border around Lazy
    Signed-off-by: Mike Szczys <szczys@hotmail.com>

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -15,4 +15,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("config.plugins")
+require("lazy").setup("config.plugins", {
+  ui = {
+    -- You can also use "single", "double", or "solid"
+    border = "rounded",
+  },
+})
