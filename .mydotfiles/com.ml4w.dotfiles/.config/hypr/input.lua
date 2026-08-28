c6730077fe83b73b20a957b489c900a044d1a06d
Mike Szczys <szczys@hotmail.com>
2026-08-27 19:51:32 -0700

    hyprland: remove touchpad middleclick
    Signed-off-by: Mike Szczys <szczys@hotmail.com>

---
+++ .mydotfiles/com.ml4w.dotfiles/.config/hypr/input.lua
@@ -0,0 +1,23 @@
+-- -----------------------------------------------------
+-- Input
+-- -----------------------------------------------------
+
+hl.config({
+    input = {
+        kb_layout    = "us",
+        kb_variant   = "",
+        kb_model     = "",
+        kb_options   = "grp:win_space_toggle",
+        kb_rules     = "",
+
+        follow_mouse = 1,
+
+        sensitivity  = 0, -- -1.0 - 1.0, 0 means no modification.
+
+        touchpad     = {
+            natural_scroll        = false,
+            clickfinger_behavior  = true,
+            middle_button_emulation = false,
+        },
+    },
+})
